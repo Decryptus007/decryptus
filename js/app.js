64e26c5b33c6beab03202dda141c9a83744e494a
Decryptus007 <domnick080@outlook.com>
2022-07-27 14:20:12 +0100

    home done

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 const ctrlSide = document.getElementById("ctrlSide");
 const sideBar = document.getElementById("sideBar");
+const copyright = document.getElementById("copyright");
+const batteryStat = document.getElementById("batteryPercent");
+const fillBattery = document.getElementById("fillBattery");
 
 let sideOpen = false;
 
@@ -18,3 +21,15 @@ ctrlSide.addEventListener("click", () => {
     sideOpen = true;
   }
 });
+
+let currYear = new Date().getFullYear();
+
+copyright.textContent = `Built with 💙 by Decryptus. ©${currYear}`;
+
+setInterval(() => {
+  navigator.getBattery().then((battery) => {
+    let res = (battery.level * 100)
+    fillBattery.style.width = `${res}%`
+    batteryStat.textContent = `${res}%`
+  });
+}, 2000);
